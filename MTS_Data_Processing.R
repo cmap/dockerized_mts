32f62a6589435d231ede953c9ad8c44ebebbc3d4
Jacob Asiedu <jasiedu@broadinstitute.org>
2020-07-21 19:20:10 -0400

    Adding project key arg

--- MTS_Data_Processing.R
+++ MTS_Data_Processing.R
@@ -22,13 +22,12 @@ if (!dir.exists(project_dir)) {dir.create(project_dir, recursive = T)}
 
 # paths to data (make sure directory of data has these files)
 path_key <- list.files(project_key_dir, pattern = "*project_key.csv", full.names = T)
-path_data <- list.files(base_dir, pattern = paste0("*", assay, "*", "_LEVEL2_MFI*"), full.names = T)
-path_cell_info <- list.files(base_dir, pattern = paste0("*", assay, "*","*_cell_info*"), full.names = T)
-path_inst_info <- list.files(base_dir, pattern = paste0("*", assay,"*", "*_inst_info"), full.names = T)
-
 print(path_key)
+path_data <- list.files(base_dir, pattern = paste0("*", assay, "*", "_LEVEL2_MFI*"), full.names = T)
 print(path_data)
+path_cell_info <- list.files(base_dir, pattern = paste0("*", assay, "*","_cell_info*"), full.names = T)
 print(path_cell_info)
+path_inst_info <- list.files(base_dir, pattern = paste0("*", assay,"*", "_inst_info"), full.names = T)
 print(path_inst_info)
 #---- Load the data ----
 
