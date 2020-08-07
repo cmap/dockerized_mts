38ea6ac4f6a971c167daaf8105d915603072a8dd
aboghossian <aboghoss@broadinstitute.org>
2020-08-07 10:30:17 -0400

    updated wrappers

--- data_wrapper.sh
+++ data_wrapper.sh
@@ -5,12 +5,17 @@ while getopts ":i:p:" arg; do
   case $arg in
     i) # specify input folder
       data_dir=${OPTARG};;
-    p) # specify project name
+    o) # specifcy output folder
+      output_dir=${OPTARG};;
+    a) # specify project name
       project=${OPTARG};;
+    p) # specify pert_name (compound)
+      comp=${OPTARG};;
   esac
 done
 
 # run docker container with arguments
 docker run -it \
   -v $data_dir:/data \
-  cmap/clue-mts-int:latest "data" "$project"
+  -v $output_dir:/results \
+  cmap/clue-mts:latest "data" "results" "$project" "$comp"
