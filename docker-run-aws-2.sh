1214eb14f3c08e2996b3a6e60226c4ea16b3be74
Jacob Asiedu <jasiedu@broadinstitute.org>
2020-08-13 15:23:43 -0400

    foo

--- docker-run-aws-2.sh
+++ docker-run-aws-2.sh
@@ -7,12 +7,11 @@ docker run \
 -v /Users/jasiedu/WebstormProjects/build:/data \
 -v /Users/jasiedu/WebstormProjects/build/results:/results \
 -v /Users/jasiedu/WebstormProjects/project_files/PMTS014_PR500:/projects \
--e projects="MTS014 Validation Compounds,German MTS013,GPER Agonist Ridky MTS013,FG-4592 Kaelin MTS013" \
+-e projects="A-1155463,German MTS013,GPER Agonist Ridky MTS013,FG-4592 Kaelin MTS013" \
 -e AWS_BATCH_JOB_ARRAY_INDEX=0 \
 -it cmap/clue-mts \
 -i /data \
--p /projects \
 -o /results \
--t "2" \
+-t "1" \
 -a "PR500"
 
