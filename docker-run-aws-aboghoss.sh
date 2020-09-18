644d816c56ce8a53431abe216dd617b2463318b4
aboghossian <aboghoss@broadinstitute.org>
2020-09-18 13:54:08 -0600

    script for local testing (andrew)

---
+++ docker-run-aws-aboghoss.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+docker run \
+--name az-test \
+-v /Users/aboghoss/Downloads/MTS014_PR300_RERUN:/data \
+-e projects='[ { "project_id": "MTS014 Validation Compounds", "pert_name": "AZ-628" }, { "project_id": "MTS014 Validation Compounds Vibliome", "pert_name": "AZ-628" }]' \
+-e AWS_BATCH_JOB_ARRAY_INDEX=0 \
+-it cmap/clue-mts \
+-i /data \
+-o /data \
+-t "1" \
+-a "PR300"
