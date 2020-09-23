68f5974be26956c7edf12c381ed13c21af287a0d
aboghossian <aboghoss@broadinstitute.org>
2020-09-23 13:59:57 -0600

    more testing

--- docker-run-pre-aboghoss.sh
+++ docker-run-pre-aboghoss.sh
@@ -3,7 +3,6 @@
 docker run \
 --name pre-test \
 -v /Users/aboghoss/Downloads/MTS014_PR300_RERUN:/data \
--e projects='[ { "project_id": "MTS014 Validation Compounds Vibliome", "pert_name": "AZ-628" } ]' \
 -e AWS_BATCH_JOB_ARRAY_INDEX=0 \
 -it cmap/clue-mts \
 -i /data \
