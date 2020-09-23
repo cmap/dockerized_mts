b6023b10e1fe3d7a8879accd64afa2a512f4b960
aboghossian <aboghoss@broadinstitute.org>
2020-09-23 11:29:23 -0600

    typo in test script

--- docker-run-aws-aboghoss.sh
+++ docker-run-aws-aboghoss.sh
@@ -3,7 +3,7 @@
 docker run \
 --name az-test \
 -v /Users/aboghoss/Downloads/MTS014_PR300_RERUN:/data \
--e projects='[ { "project_id": "MTS014 Validation Compounds", "pert_name": "AZ-628" }, { "project_id": "MTS014 Validation Compounds Vibliome", "pert_name": "AZ-628" }]' \
+-e projects='[ { "project_id": "MTS014 Validation Compounds", "pert_name": "AZ-628" }, { "project_id": "MTS014 Validation Compounds Vibliome", "pert_name": "AZ-628" } ]' \
 -e AWS_BATCH_JOB_ARRAY_INDEX=0 \
 -it cmap/clue-mts \
 -i /data \
