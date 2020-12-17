47d980bdcdbdfaa32e652b007f2ae735a4fa7321
aboghossian <aboghoss@broadinstitute.org>
2020-12-17 15:48:38 -0500

    finalized no QC option

--- docker-run-aws-aboghoss.sh
+++ docker-run-aws-aboghoss.sh
@@ -4,9 +4,9 @@ docker run \
 --rm \
 --name az-test-2 \
 -v /Users/aboghoss/Downloads/MTS015_PR500:/data \
--e projects='[ { "project_id": "MTS015 DMC - Amgen", "pert_name": "ATA6" } ]' \
+-e projects='[ { "project_id": "Validation Compounds", "pert_name": "anagrelide" } ]' \
 -e AWS_BATCH_JOB_ARRAY_INDEX=0 \
--it cmap/clue-mts \
+-it aboghoss/clue-mts:dev \
 -i /data \
 -o /data \
 -t "1" \
