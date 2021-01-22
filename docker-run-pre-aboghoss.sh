c76407ed9244615ca507b030c71626864f4019c6
aboghossian <aboghoss@broadinstitute.org>
2021-01-22 09:04:51 -0500

    local processing changes

--- docker-run-pre-aboghoss.sh
+++ docker-run-pre-aboghoss.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 docker run \
---name pre-test \
--v /Users/aboghoss/Downloads/MTS015_PR300:/data \
+--name pre-processing \
+-v /Users/aboghoss/Downloads/MTS015_PR500_reprocess:/data \
 -e AWS_BATCH_JOB_ARRAY_INDEX=0 \
 -it cmap/clue-mts \
 -i /data \
 -o /data \
 -t "0" \
 -p /data \
--a "PR300"
+-a "PR500"
