8c4555192075e85e54d0170b37dca38462ad340f
aboghossian <aboghoss@broadinstitute.org>
2021-10-21 09:56:38 -0400

    removed batch correction and growth rate from LFC

--- lfc/aws_batch.sh
+++ lfc/aws_batch.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 # read in flagged arguments
-while getopts ":b:o:g:" arg; do
+while getopts ":b:o:" arg; do
   case $arg in
     b) # specify input folder
       data_dir=${OPTARG};;
     o) # specifcy output folder
       output_dir=${OPTARG};;
-    g) # specify assay/build (PR300 or PR500)
-      calc_gr=${OPTARG}
   esac
 done
 
@@ -16,7 +14,7 @@ chmod +x /calc_lfc.R
 chmod +x /src/lfc_functions.R
 export HDF5_USE_FILE_LOCKING=FALSE
 echo "${data_dir}" "${output_dir}" "${calc_gr}"
-Rscript /calc_lfc.R -b "${data_dir}" -o "${output_dir}" -g "${calc_gr}"
+Rscript /calc_lfc.R -b "${data_dir}" -o "${output_dir}"
 
 exit_code=$?
 
