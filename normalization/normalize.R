98b88b5e28e363ac446df09741f82454e251db01
aboghossian <aboghoss@broadinstitute.org>
2022-02-11 09:42:00 -0500

    fixed merge with dev branch

--- normalization/normalize.R
+++ normalization/normalize.R
@@ -42,24 +42,8 @@ cell_info <- data.table::fread(path_cell_info) %>%
   dplyr::mutate(pool_id = ifelse(pool_id == "" | pool_id == -666,
                                  "CTLBC", pool_id))
 
-<<<<<<< HEAD
-# make long version of platemap
-inst_info <- data.table::fread(path_inst_info) %>%
-  dplyr::filter(!str_detect(prism_replicate, "BASE"), !is_well_failure) %>%
-  make_long_map(.) %>%
-  dplyr::mutate(pert_dose = ifelse(pert_dose >= 0.001, round(pert_dose, 4), pert_dose),
-                pert_idose = paste(pert_dose, word(pert_idose, 2)),
-                pert_idose = ifelse(pert_idose == "NA NA", NA, pert_idose))
-
-# change validation (.es) to treatment for processing
-inst_info$pert_type[which(str_detect(inst_info$pert_type, "poscon.es"))] <-
-  "trt_cp"
-inst_info$pert_type[which(inst_info$pert_type == "trt_cpd")] <-
-  "trt_cp"
-=======
 # read in inst_info
 inst_info <- data.table::fread(path_inst_info)
->>>>>>> dev
 
 # split out base plate
 base_day <- data.table::fread(path_inst_info) %>%
@@ -73,68 +57,7 @@ master_logMFI <- log2(raw_matrix) %>%
   reshape2::melt(varnames = c("rid", "profile_id"), value.name = "logMFI") %>%
   dplyr::filter(is.finite(logMFI)) %>%
   dplyr::inner_join(cell_info) %>%
-<<<<<<< HEAD
-  dplyr::inner_join(inst_info) %>%
-  dplyr::select(profile_id, rid, ccle_name, pool_id, culture, prism_replicate, pert_time,
-                pert_type, pert_dose, pert_idose, pert_mfc_id, pert_name, pert_well,
-                logMFI, project_id)
-
-compounds_logMFI <- master_logMFI %>%
-  dplyr::filter(pert_type == "trt_cp")
-
-controls_logMFI <- master_logMFI %>%
-  dplyr::filter(pert_type != "trt_cp") %>%
-  dplyr::mutate(project_id = "controls")
-
-varied_compounds <- compounds_logMFI %>%
-  dplyr::distinct(pert_name, pert_idose, project_id, prism_replicate) %>%
-  dplyr::group_by(pert_name, project_id, prism_replicate) %>%
-  dplyr::summarise(n = n(), .groups = "drop") %>%
-  dplyr::filter(n > 1) %>%
-  dplyr::mutate(full_curve = n > 4)
-
-# handles multiple anchor doses
-curve_comps <- varied_compounds %>%
-  dplyr::filter(full_curve)
-non_curve_comps <- varied_compounds %>%
-  dplyr::filter(!full_curve | n > 9, n > 1)
-compounds_logMFI %<>%
-  dplyr::group_by(profile_id, rid, ccle_name, pool_id, culture, prism_replicate,
-                  pert_type, pert_well, pert_time, logMFI, project_id) %>%
-  dplyr::mutate(n = n()) %>%
-  dplyr::ungroup()
-if (nrow(non_curve_comps) > 0) {
-  comps_with_dose <- non_curve_comps %>%
-    dplyr::select(-n) %>%
-    dplyr::inner_join(compounds_logMFI) %>%
-    dplyr::mutate(pert_name = ifelse(n > 1,
-                                     paste(pert_name, pert_idose, sep = "_"),
-                                     pert_name))
-  compounds_logMFI %<>%
-    dplyr::anti_join(non_curve_comps %>% dplyr::select(-n)) %>%
-    dplyr::bind_rows(comps_with_dose)
-}
-
-compounds_logMFI %<>%
-  dplyr::group_by(profile_id, rid, ccle_name, pool_id, culture, prism_replicate,
-                  pert_type, pert_well, pert_time, logMFI, project_id, n) %>%
-  dplyr::summarise(pert_dose = ifelse(any(pert_name %in% curve_comps$pert_name),
-                                      pert_dose[pert_name %in% curve_comps$pert_name],
-                                      pert_dose),
-                   pert_idose = ifelse(any(pert_name %in% curve_comps$pert_name),
-                                       pert_idose[pert_name %in% curve_comps$pert_name],
-                                       pert_idose),
-                   pert_mfc_id = ifelse(any(pert_name %in% curve_comps$pert_name),
-                                        pert_mfc_id[pert_name %in% curve_comps$pert_name],
-                                        pert_mfc_id),
-                   pert_name = paste(sort(unique(pert_name)), collapse = "_"),
-                   .groups = "drop") %>%
-  dplyr::select(-n)
-
-master_logMFI <- dplyr::bind_rows(compounds_logMFI, controls_logMFI)
-=======
   dplyr::inner_join(inst_info)
->>>>>>> dev
 
 # create barcode tables
 barcodes <- master_logMFI %>%
@@ -167,7 +90,7 @@ if(nrow(logMFI_base) > 0) {
     dplyr::mutate(rLMFI = mean(rLMFI)) %>%
     dplyr::ungroup() %>%
     dplyr::distinct(rid, rLMFI)
-
+  
   base_normalized <- logMFI_base %>%
     dplyr::filter(is.finite(logMFI), logMFI != 0) %>%
     dplyr::left_join(logMFI_profile) %>%
