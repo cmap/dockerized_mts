c1c2b6aea72e3c16266867d976a2f200a3b4d082
NoopDawg <APJonchhe@gmail.com>
2022-02-16 13:20:08 -0500

    fix: fix instinfo being output as csv and including index

--- deal/build_docker.sh
+++ deal/build_docker.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 #change the version number for each new build
-docker build -t prismcmap/deal:latest -t prismcmap/deal:v0.0.1 --rm=true .
+docker build -t prismcmap/deal:latest -t prismcmap/deal:v0.0.2 --rm=true .
