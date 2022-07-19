f6b673360ac9c3af24bdc8ccbd60890953079739
AnupJonchhe <anup@broadinstitute.org>
2022-07-19 09:28:00 -0400

    Map validator to check for spaces and lowercase

--- map_validator/build_docker.sh
+++ map_validator/build_docker.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker build --platform linux/amd64 -t map-validator:latest -t map-validator:v0.0.1 --rm=true .
+docker build --platform linux/amd64 -t map-validator:latest -t map-validator:v0.1.0 --rm=true .
--- map_validator/jenkins_map_validator.py
+++ map_validator/jenkins_map_validator.py
@@ -19,15 +19,26 @@ def build_parser():
 
 def main(args):
 
-    missing_fields = map_validator.validate(args.map,args.verbose)
+    missing_fields = map_validator.validate_required_fields(args.map, args.verbose)
+    failed_perts = map_validator.validate_pert_ids(args.map, args.verbose)
 
     if (len(missing_fields) > 0):
         errorMessage = args.map + ' missing following required fields: {}'.format(', '.join(missing_fields))
         print(errorMessage)
         sys.exit(-1)
-    else:
-        print('map validated successfully')
-        sys.exit(0)
+    if (len(failed_perts) > 0):
+        for errorMessage in failed_perts:
+            print("i: {i}, pert_id: {pert_id}, reason: {reason}".format(
+                errorMessage.i,
+                errorMessage.pert_id,
+                errorMessage.reason
+            ))
+        sys.exit(-1)
+    print('map validated successfully')
+    sys.exit(0)
+
+
+
 
 if __name__ == "__main__":
     args = build_parser().parse_args(sys.argv[1:])
