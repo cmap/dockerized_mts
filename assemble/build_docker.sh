e9d514896e9f11458f6dc3ce9d5a3998171e036d
Anup Jonchhe <APJonchhe@gmail.com>
2022-09-06 11:18:51 -0400

    chore: bump version numbers

--- assemble/build_docker.sh
+++ assemble/build_docker.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+REPO="prismcmap"
+NAME="assemble"
+
+VERSION="v1.0.1" #change the version number for each new build
+TAG="latest"
 
 #change the version number for each new build
-docker build --platform linux/amd64 -t prismcmap/assemble:latest -t prismcmap/assemble:v1.0.0 --rm=true .
+docker build --platform linux/amd64 -t $REPO/$NAME:$TAG -t $REPO/$NAME:$VERSION --rm=true .
+
+docker push $REPO/$NAME:$VERSION
+docker push $REPO/$NAME:$TAG
--- assemble/docker_push_tags.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-docker push prismcmap/assemble:v1.0.0
-docker push prismcmap/assemble:latest
