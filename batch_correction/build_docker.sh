fe49d2fbfd5c1d34aa8abf2929c6b15159cfc6a9
Anup Jonchhe <APJonchhe@gmail.com>
2022-12-06 09:31:05 -0800

    chore: move dockers from cmap to prismcmap organization

--- batch_correction/build_docker.sh
+++ batch_correction/build_docker.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker build --platform linux/amd64 -t cmap/batch-correct-module:dev -t cmap/batch-correct-module:v0.0.1 --rm=true .
+docker build --platform linux/amd64 -t prismcmap/batch-correct-module:latest -t prismcmap/batch-correct-module:v0.0.1 --rm=true .
