299601fbcd15b6f2acf8d398c863de1d8d3bd927
shiker1738 <shiker@broadinstitute.org>
2022-12-14 12:12:40 -0500

    updated mutation visualization for single dose

--- reports/build_docker.sh
+++ reports/build_docker.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 #change the version number for each new build
-docker build --platform linux/amd64 -t prismcmap/reports:latest -t prismcmap/reports:v0.2.0 --rm=true .
+docker build --platform linux/amd64 -t prismcmap/reports:latest -t prismcmap/reports:v0.2.1 --rm=true .
--- reports/docker_push_tags.sh
+++ reports/docker_push_tags.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-docker push prismcmap/reports:v0.2.0
+docker push prismcmap/reports:v0.2.1
 docker push prismcmap/reports:latest
