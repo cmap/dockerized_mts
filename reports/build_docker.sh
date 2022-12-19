f36ba1dcd527cee6075265de9ff24137f951803e
shiker1738 <shiker@broadinstitute.org>
2022-12-19 13:40:32 -0500

    legend changes

--- reports/build_docker.sh
+++ reports/build_docker.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 #change the version number for each new build
-docker build --platform linux/amd64 -t prismcmap/reports:latest -t prismcmap/reports:v0.2.3 --rm=true .
+docker build --platform linux/amd64 -t prismcmap/reports:latest -t prismcmap/reports:v0.2.4 --rm=true .
--- reports/docker_push_tags.sh
+++ reports/docker_push_tags.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-docker push prismcmap/reports:v0.2.3
+docker push prismcmap/reports:v0.2.4
 docker push prismcmap/reports:latest
