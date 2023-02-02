507cc59e3e9b57ad51003ee3585222dd6e8dd412
shiker1738 <shiker@broadinstitute.org>
2023-02-02 16:55:34 -0500

    updated version

--- reports/docker_build_and_push.sh
+++ reports/docker_build_and_push.sh
@@ -2,7 +2,7 @@
 REPO="prismcmap"
 NAME="reports"
 
-VERSION="v0.2.8" #change the version number for each new build
+VERSION="v0.2.9" #change the version number for each new build
 TAG="latest"
 
 #change the version number for each new build
