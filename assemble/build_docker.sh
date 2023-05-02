c1cea0daa3007059ad6649b78cec8f27d8f0ae19
AnupJonchh <anup@broadinstitute.org>
2023-05-02 16:16:39 -0400

    updated docker build to move conda env creation to base image

--- assemble/build_docker.sh
+++ assemble/build_docker.sh
@@ -2,11 +2,13 @@
 REPO="prismcmap"
 NAME="assemble-db"
 
-VERSION="v1.0.1" #change the version number for each new build
+VERSION="v1.0.2" #change the version number for each new build
 TAG="latest"
 
 #change the version number for each new build
 docker build --platform linux/amd64 -t $REPO/$NAME:$TAG -t $REPO/$NAME:$VERSION --rm=true .
+#
+#docker push $REPO/$NAME:$VERSION
+#docker push $REPO/$NAME:$TAG
 
-docker push $REPO/$NAME:$VERSION
-docker push $REPO/$NAME:$TAG
+#v1.0.2 - changed base Dockerfile to include conda env
---
+++ assemble/docker-base/docker_build.sh
@@ -0,0 +1,1 @@
+docker build --platform linux/amd64 -t prismcmap/base-merino:latest -t prismcmap/base-merino:v0.0.2 --rm=true .
