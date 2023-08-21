5460125e7b7327d3a830faca673841e6a2f5e8cb
AnupJonchh <anup@broadinstitute.org>
2023-08-21 12:14:41 -0400

    adding CSL search_patterns

--- merge-csv-files/merge-csvs-bash.sh
+++ merge-csv-files/merge-csvs-bash.sh
@@ -8,16 +8,21 @@ python setup.py develop
 #return to /
 cd /
 
+print_help () {
+  python /clue/bin/merge_csvs.py --help
+  echo "  -spl, --search_pattern_list - comma separated list of search patterns in DRC, e.g. 'DRC_TABLE*,discrete_associations*' (bash)"
+}
+
 #optional
 if test $# -lt 1; then
-  python /clue/bin/merge_csvs.py --help
+  print_help
   exit 1
 fi
 
 while test $# -gt 0; do
   case "$1" in
     -h|--help)
-      python /clue/bin/merge_csvs.py --help
+      print_help
       exit 0
       ;;
     -f|--compound_key_path)
@@ -32,9 +37,17 @@ while test $# -gt 0; do
       shift
       OUT_DIR=$1
       ;;
+    -n|--filename)
+      shift
+      OUT_FILE=$1
+      ;;
     -s|--search_pattern)
       shift
       PATTERN=$1
+      ;;
+    -spl|--search_pattern_list)
+      shift
+      SEARCH_PATTERN_LIST=$1
       ;;
     -sp|--separator)
       shift
@@ -63,6 +76,8 @@ if [[ ! -z "${AWS_BATCH_JOB_ARRAY_INDEX}" ]]; then
   batch_index=${AWS_BATCH_JOB_ARRAY_INDEX}
 fi
 
+args=()
+
 if [[ ! -z $projects ]]
 then
     PROJECT=$(cat "${projects}" | jq -r --argjson index ${batch_index} '.[$index].x_project_id')
@@ -81,6 +96,7 @@ then
         DATA_DIR="${DATA_DIR}"/"${PROJECT,,}"/"${PROJECT^^}"/"${PERT_PLATE}"/"${sanitized_pert_id}"/biomarker
         OUT_DIR="${OUT_DIR}"/"${PROJECT,,}"/"${PROJECT^^}"/"${PERT_PLATE}"/"${sanitized_pert_id}"
     fi
+
 fi
 
 if [[ ! -d $OUT_DIR ]]
@@ -88,10 +104,9 @@ then
   mkdir -p $OUT_DIR
 fi
 
-args=(
+args+=(
   -d "$DATA_DIR"
   -o "$OUT_DIR"
-  -s "$PATTERN"
 )
 
 if [[ ! -z $ADD_PROJECT_NAME ]]
@@ -112,11 +127,39 @@ then
   )
 fi
 
+if [[ ! -z $OUT_FILE ]]
+then
+  args+=(
+    -n "$OUT_FILE"
+  )
+fi
+
 if [[ "$pert_id" == "DMSO" ]]
 then
   echo "Skipping DMSO"
 else
   python /clue/bin/merge_csvs.py "${args[@]}"
+fi
+
+if [[ ! -z $SEARCH_PATTERN_LIST ]]
+then
+  IFS=',' read -r -a array <<< "$SEARCH_PATTERN_LIST"
+  for pattern in "${array[@]}"
+  do
+    python /clue/bin/merge_csvs.py "${args[@]}" -s "$pattern"
+  done
+
+else
+  args+=(
+    -s "$PATTERN"
+  )
+  if [[ "$pert_id" == "DMSO" ]]
+  then
+    echo "Skipping DMSO"
+  else
+    python /clue/bin/merge_csvs.py "${args[@]}"
+  fi
+
 fi
 
 exit_code=$?
--- merge-csv-files/merge_csvs.py
+++ merge-csv-files/merge_csvs.py
@@ -14,6 +14,7 @@ def build_parser():
     parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--data_dir', '-d', help="Path to directory containing csv files to concat", type=str, required=True)
     parser.add_argument('--out', '-o', help='Output directory', type=str, required=True)
+    parser.add_argument('--outfile', '-n', help='Output filename', type=str, required=False)
     parser.add_argument('--search_pattern', '-s', help="search pattern for csv's",default='*', type=str, required=True)
     parser.add_argument('--separator', '-sp', help="File sperator defaults to ','", default=',',type=str)
     parser.add_argument('--addProjectName', '-ap', help="Whether to prepend the project name to the file", action="store_true", default=False)
@@ -28,7 +29,10 @@ def main(args):
         project_name = os.path.basename(args.data_dir)
         output_file = os.path.join(args.out,project_name + "_" + args.search_pattern)
     else:
-        output_file = os.path.join(args.out, args.search_pattern).replace("*",".csv")
+        if args.outfile:
+            output_file = os.path.join(args.out, args.outfile)  # filename override
+        else:
+            output_file = os.path.join(args.out, args.search_pattern).replace("*","") + ".csv" # expecting: "DRC_TABLE*"
 
     matches = glob.glob(search_str)
     print("Found {} files".format(len(matches)))
