ae7074d61839408d6bca9657dbc2d725749c721d
AnupJonchh <anup@broadinstitute.org>
2023-09-21 17:59:11 -0400

    pushing testing

--- reports/docker_build_and_push.sh
+++ reports/docker_build_and_push.sh
@@ -2,11 +2,12 @@
 REPO="prismcmap"
 NAME="reports"
 
-VERSION="v0.3.0" #change the version number for each new build
-TAG="test_unify"
+#VERSION="v0.3.0" #change the version number for each new build
+TAG="testing"
 
 #change the version number for each new build
-docker build --platform linux/amd64 -t $REPO/$NAME:$TAG -t $REPO/$NAME:$VERSION --rm=true .
+docker build --platform linux/amd64 -t $REPO/$NAME:$TAG \
+--rm=true .
 
-docker push $REPO/$NAME:$VERSION
+#docker push $REPO/$NAME:$VERSION
 docker push $REPO/$NAME:$TAG
