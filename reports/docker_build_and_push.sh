61d7d2b0eb9b285a31d44d26d0ff671b7868f2b0
maha-naim <naim@broadinstitute.org>
2023-09-26 14:55:46 -0400

    Adding flexibility to deal to only expect files that exist and changing prism_replicate to just replicate

--- reports/docker_build_and_push.sh
+++ reports/docker_build_and_push.sh
@@ -2,11 +2,10 @@
 REPO="prismcmap"
 NAME="reports"
 
-VERSION="v0.2.11" #change the version number for each new build
-TAG="latest"
+#VERSION="v0.2.11" #change the version number for each new build
+TAG="testing"
 
 #change the version number for each new build
-docker build --platform linux/amd64 -t $REPO/$NAME:$TAG -t $REPO/$NAME:$VERSION --rm=true .
+docker build --platform linux/amd64 -t $REPO/$NAME:$TAG --rm=true .
 
-docker push $REPO/$NAME:$VERSION
 docker push $REPO/$NAME:$TAG
