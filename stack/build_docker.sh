2eb3d9215e1bf0a46b9458b0a70ad0ccfbdb70e6
maha-naim <naim@broadinstitute.org>
2023-11-02 14:28:09 -0400

    Adjusting keys passed for extended builds during stack

--- stack/build_docker.sh
+++ stack/build_docker.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 #change the version number for each new build
-docker build --platform linux/amd64 -t prismcmap/stack:latest -t prismcmap/stack:v0.2.0 --rm=true .
+docker build --platform linux/amd64 -t prismcmap/stack:develop --rm=true .
