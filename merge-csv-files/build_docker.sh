5f2af26834bd61670f91994df0d231f3511a183c
AnupJonchhe <anup@broadinstitute.org>
2024-06-26 19:41:05 -0400

    feat: merging files in a stream processed to reduce memory requirements

--- merge-csv-files/build_docker.sh
+++ merge-csv-files/build_docker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 #change the version number for each new build
-docker build --platform linux/amd64 -t prismcmap/merge-csv-files:test --rm=true .
+docker build --platform linux/amd64 -t prismcmap/merge-csv-files:testing --rm=true .
 
-docker push prismcmap/merge-csv-files:test
+docker push prismcmap/merge-csv-files:testing
