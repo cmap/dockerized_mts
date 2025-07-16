918a2236d68871e8efd5742a4d9310ace4ee934a
jdavis3141 <jdavis@broadinstitute.org>
2025-07-16 08:01:02 -0400

    trying to fix prism-base image, point to archived buster repos

--- assemble/docker-base/docker_build.sh
+++ assemble/docker-base/docker_build.sh
@@ -1,1 +1,1 @@
-docker build --platform linux/amd64 -t prismcmap/base-prism:latest -t prismcmap/base-prism:v0.0.3 --rm=true .
+podman build --platform linux/amd64 -t prismcmap/base-prism:latest -t prismcmap/base-prism:v0.0.3 --rm=true .
